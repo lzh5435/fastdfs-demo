7942bd97a58045417e84b716020c4a4fec9dc1be
lzh5435 <lzh5435@163.com>
2020-07-11 20:18:00 +0800

    完成 安装和java测试

---
+++ src/test/java/test/liuzehua/fastdfsdemo/FastdfsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package test.liuzehua.fastdfsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastdfsDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
